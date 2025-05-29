0ef8ece445ab81f3e7a373133501ac1ac1997bd0
dineshkumar24v <dineshkumar24v@gmail.com>
2025-05-29 09:35:46 +0530

    updating packages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/react_task/', // This must match your repo name
   plugins: [react()],
 })
